de5ec57c62ef21a4203619984eaa0e7ec5d8c6fc
Kira McLean <email@kiramclean.com>
2022-02-04 21:44:40 -0700

    get list of place names from OS names API on search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,60 @@
+import React, { useRef, useEffect, useState } from 'react';
+
 import './Search.css';
 
 function Search() {
-  const onSubmit = e => {
-    e.preventDefault();
-    console.log("Submitting search...", e)
+  const nameServiceUrl = 'https://api.os.uk/search/names/v1';
+
+  const [results, setResults] = useState([])
+  const [query, setQuery] = useState("");
+
+  const OSapiKey = process.env.REACT_APP_OS_API_KEY;
+  const OSplacesServiceBase = 'https://api.os.uk/search/names/v1/find' ;
+  /* 'https://api.os.uk/search/places/v1/find' */
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const url = OSplacesServiceBase +
+                "?maxresults=10" +
+                "&key=" + OSapiKey +
+                "&query=" + query
+    const places = await fetch(url).then(response => response.json());
+    setResults(places.results);
   }
 
+  const resultsToListItems = () => {
+    return results.map((result) => {
+      const entry = result.GAZETTEER_ENTRY;
+      // TODO: Make these clickable links, reverse geocode on click
+      // and zoom map in to that area
+      return (<li>{entry.NAME1}, {entry.REGION}</li>);
+  })};
+
+  const resultsList = () => {
+      return results.length > 0 && resultsToListItems();
+  };
+
   return (
-    <form action="/"
-          method="get"
-          className="Search-container"
-          autoComplete="off"
-          onSubmit={onSubmit}>
-      <label htmlFor="header-search">
-        <span className="visually-hidden">Search for a place</span>
-      </label>
-      <input
-        type="text"
-        id="header-search"
-        placeholder="Search for a place"
-        name="s"
-        className="Search-input"
-      />
-      <button type="submit" className="Search-submit-button">Search</button>
-    </form>
+    <div className="Search-container">
+      <form action="/"
+            method="get"
+            autoComplete="off"
+            onSubmit={onSubmit}>
+        <label htmlFor="header-search">
+          <span className="visually-hidden">Search for a place</span>
+        </label>
+        <input
+          type="text"
+          id="header-search"
+          placeholder="Search for a place"
+          name="s"
+          className="Search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="Search-submit-button">Go</button>
+      </form>
+      {resultsList()}
+    </div>
   );
 }
 
