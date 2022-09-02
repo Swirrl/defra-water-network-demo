0d7556977b6f4c206730165ca336fd1b44235e0b
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2022-09-02 12:59:47 +0100

    add react router so we can find a particular watercourse link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
 import Map from "./components/Map";
 
 function App() {
   return (
     <div>
-      <Map />
+      <div>
+        <Routes>
+          <Route path="/watercourse-link/:watercourseLinkId" element={<Map />}/> 
+          <Route path="*" element={<Map />} />
+        </Routes>
+      </div>
     </div>
   );
 }
